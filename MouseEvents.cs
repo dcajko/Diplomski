b4c733156c4f84672a7f0693f492b179471a0dac
dorian <someoneu0@gmail.com>
2024-07-28 19:38:53 +0200

    Mouse event implementation fail

---
+++ MouseEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomski
+{
+    [Flags]
+    public enum MouseEvents
+    {
+        None = 0,
+        LeftDown = 1 << 0,
+        LeftUp = 1 << 1,
+        LeftDragStart = 1 << 2,
+        LeftDragEnd = 1 << 3,
+        RightDown = 1 << 4,
+        RightUp = 1 << 5,
+        RightDragStart = 1 << 6,
+        RightDragEnd = 1 << 7,
+    }
+}
--- View.cs
+++ View.cs
@@ -7,10 +7,13 @@ using System.Runtime.CompilerServices;
 
 public partial class View : FreeLookCameraBase
 {
-    private bool casting = false;
+    private bool buttonHeld = false;
     private Vector3 firstLocation;
     private Vector3 lastLocation;
     private bool isShiftHeld;
+    private float distanceMoved;
+    private MouseEvents mouseEvents = MouseEvents.None;
+    private InputEventMouseButton lastMouseInputEvent;
 
     [Signal]
     public delegate void SelectionClickEventHandler(Vector3 location);
@@ -20,6 +23,8 @@ public partial class View : FreeLookCameraBase
     public delegate void SelectionResizeEventHandler(Vector3 location);
     [Signal]
     public delegate void SelectionEndedEventHandler(Vector3 location);
+    [Signal]
+    public delegate void MouseEventEventHandler(Vector3 location, MouseEvents mouseEvents);
     public List<Pawn> pawns { get; private set; }
 
     public override void _Ready()
@@ -28,10 +33,70 @@ public partial class View : FreeLookCameraBase
         //this.GetViewport().DebugDraw = Viewport.DebugDrawEnum.Overdraw;
     }
 
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        // TODO: Optimize; don't fire each tick
+        if (buttonHeld)
+        {
+            lastLocation = ShootRay().Item1;
+            if (firstLocation.DistanceSquaredTo(lastLocation) > 1)
+            {
+                EmitSignal(SignalName.SelectionResize, lastLocation);
+            }
+        }
+    }
+
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
+        ProcessMouseInput(@event);
 
+        //if (@event is InputEventMouseButton mouseEvent)
+        //{
+        //    if (mouseEvent.ButtonIndex == MouseButton.Left)
+        //    {
+        //        if (mouseEvent.Pressed)
+        //        {
+        //            if (isShiftHeld)
+        //            {
+        //                casting = true;
+        //                firstLocation = ShootRay().Item1;
+        //                EmitSignal(SignalName.SelectionStarted, firstLocation);
+        //            }
+        //        }
+        //        else // Mouse button is released
+        //        {
+        //            if (casting)
+        //            {
+        //                casting = false;
+        //                EmitSignal(SignalName.SelectionEnded, lastLocation);
+        //            }
+        //            else
+        //            {
+        //                EmitSignal(SignalName.SelectionClick);
+        //                var targetLocation = ShootRay();
+        //                var formationBox = new FormationBox();
+        //                formationBox.MinSize = 2;
+        //                formationBox.Position = targetLocation.Item1;
+        //                int selectedPawnsCount = pawns.Where(x => x.Selected).Count();
+        //                formationBox.Prepare(selectedPawnsCount);
+        //                var points = formationBox.PreparePoints();
+        //                Helper.AddNode(GetTree().Root, formationBox, "FormationBox");
+
+        //                if (points.Count > 0)
+        //                {
+        //                    SelectedToLocation(targetLocation.Item1, points);
+        //                }
+        //                lastLocation = targetLocation.Item1;
+        //            }
+        //        }
+        //    }
+        //}
+    }
+
+    private void ProcessMouseInput(InputEvent @event)
+    {
         if (@event is InputEventKey keyEvent)
         {
             if (keyEvent.Keycode == Key.Shift && keyEvent.Pressed)
@@ -43,49 +108,62 @@ public partial class View : FreeLookCameraBase
                 isShiftHeld = false;
             }
         }
-
-        if (@event is InputEventMouseButton mouseEvent)
+        if (@event is InputEventMouseButton e)
         {
-            if (mouseEvent.ButtonIndex == MouseButton.Left)
+            lastMouseInputEvent = e;
+            var clickWorldLocation = ShootRay();
+            buttonHeld = e.Pressed;
+
+            if (mouseEvents == MouseEvents.None)
             {
-                if (mouseEvent.Pressed)
+                lastLocation = clickWorldLocation.Item1;
+                firstLocation = clickWorldLocation.Item1;
+                EmitSignal(SignalName.SelectionStarted, firstLocation);
+            }
+
+            if (clickWorldLocation.Item1.DistanceSquaredTo(lastLocation) > 1)
+            {
+                if (e.ButtonIndex == MouseButton.Left)
                 {
-                    if (isShiftHeld)
-                    {
-                        casting = true;
-                        firstLocation = ShootRay();
-                        EmitSignal(SignalName.SelectionStarted, firstLocation);
-                    }
+                    //GD.Print(e.Pressed ? "Left pressed" : "Left released");
+                    mouseEvents = e.Pressed
+                        ? mouseEvents | MouseEvents.LeftDragStart
+                        : mouseEvents ^ MouseEvents.LeftDragStart;
                 }
-                else // Mouse button is released
+                else if (e.ButtonIndex == MouseButton.Right)
                 {
-                    if (casting)
-                    {
-                        casting = false;
-                        EmitSignal(SignalName.SelectionEnded, lastLocation);
-                    }
-                    else
-                    {
-                        EmitSignal(SignalName.SelectionClick);
-                        var targetLocation = ShootRay();
-                        var formationBox = new FormationBox();
-                        formationBox.MinSize = 2;
-                        formationBox.Position = targetLocation;
-                        int selectedPawnsCount = pawns.Where(x => x.Selected).Count();
-                        formationBox.Prepare(selectedPawnsCount);
-                        var points = formationBox.PreparePoints();
-                        Helper.AddNode(GetTree().Root, formationBox, "FormationBox");
-                        
-                        if(points.Count > 0)
-                        {
-                            SelectedToLocation(targetLocation, points);
-                        }
-                        lastLocation = targetLocation;
-                    }
+                    //GD.Print(e.Pressed ? "Right pressed" : "Right released");
+                    mouseEvents = e.Pressed
+                        ? mouseEvents | MouseEvents.RightDragStart
+                        : mouseEvents ^ MouseEvents.RightDragStart;
                 }
             }
+            else
+            {
+                if (e.ButtonIndex == MouseButton.Left)
+                {
+                    //GD.Print(e.Pressed ? "Left pressed" : "Left released");
+                    mouseEvents = e.Pressed
+                        ? mouseEvents | MouseEvents.LeftDown
+                        : mouseEvents ^ MouseEvents.LeftDown;
+                }
+                else if (e.ButtonIndex == MouseButton.Right)
+                {
+                    //GD.Print(e.Pressed ? "Right pressed" : "Right released");
+                    mouseEvents = e.Pressed
+                        ? mouseEvents | MouseEvents.RightDown
+                        : mouseEvents ^ MouseEvents.RightDown;
+                }
+            }
+            if (mouseEvents == MouseEvents.None)
+            {
+                EmitSignal(SignalName.SelectionEnded, lastLocation);
+            }
+            GD.Print(mouseEvents);
+            lastLocation = clickWorldLocation.Item1;
         }
     }
+
     private void SelectedToLocation(Vector3 loaction, List<Vector3> points)
     {
         int i = 0;
@@ -99,21 +177,7 @@ public partial class View : FreeLookCameraBase
         }
     }
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-        // TODO: Optimize; don't fire each tick
-        if (casting)
-        {
-            lastLocation = ShootRay();
-            if (firstLocation.DistanceSquaredTo(lastLocation) > 1)
-            {
-                EmitSignal(SignalName.SelectionResize, lastLocation);
-            }
-        }
-    }
-
-    public Vector3 ShootRay()
+    public (Vector3, Pawn) ShootRay()
     {
         var mousePosition = this.GetViewport().GetMousePosition();
         int rayLength = 1000;
@@ -124,14 +188,21 @@ public partial class View : FreeLookCameraBase
         rayQuery.From = from;
         rayQuery.To = to;
         var raycastResult = space.IntersectRay(rayQuery);
-        //if (raycastResult != null) 
-        //{
-        //    var collider = raycastResult["collider"].AsGodotObject();
-        //    if (collider is Pawn pawn)
-        //    {
-        //        pawn.Selected = true;
-        //    }
-        //}
-        return (Vector3)raycastResult["position"];
+        if (raycastResult != null)
+        {
+            if (raycastResult.TryGetValue("position", out var position))
+            {
+
+                if (raycastResult.TryGetValue("collider", out var collider) && collider.AsGodotObject() is Pawn pawn)
+                {
+                    return ((Vector3)position, pawn);
+                }
+                else
+                {
+                    return ((Vector3)position, null);
+                }
+            }
+        }
+        return (Vector3.Zero, null);
     }
 }
